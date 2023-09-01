67d7e8349d7d4ccf82213f118b755a4c6c98c8d8
Maximiliano Narea <maximiliano@platan.us>
2023-09-01 11:42:28 -0400

    feat(App): add imports, interfaces and styles

--- app/javascript/api/comments.ts
+++ app/javascript/api/comments.ts
@@ -1,5 +1,23 @@
+import type { AxiosResponse } from 'axios';
+import type { Merge } from 'type-fest';
+import api from './index';
+
 export interface Comment {
   id: number;
   body: string;
 }
 
+export default {
+  create(body: string, feedbackSessionId: number) {
+    const path = `/app/feedback_sessions/${feedbackSessionId}/comments`;
+
+    return api<unknown, Merge<AxiosResponse, { data: { body: string, feedbackSessionId: number }}>>({
+      method: 'post',
+      url: path,
+      data: {
+        body,
+        feedbackSessionId,
+      },
+    });
+  },
+};
